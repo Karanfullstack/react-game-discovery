48f6db7ebcea0f2c2c3acaa11c36e950f1448d6a
Karan Chauhan <karanchouhan211@gmail.com>
2024-04-29 12:08:21 +0200

    removing duplicate interfaces

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -2,12 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { FetchProps } from "../services/api-client";
 import apiClient from "../services/api-client";
+import { type Platform } from "./usePlatforms";
 
-export interface Platform {
-	id: number;
-	name: string;
-	slug: string;
-}
+
 
 export interface Game {
 	id: number;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
-import { Platform } from "./useGame";
 import { FetchProps } from "../services/api-client";
 import { platformData } from "../data/platformData";
 
-// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+export interface Platform {
+	id: number;
+	name: string;
+	slug: string;
+}
 
 const usePlatforms = () =>
 	useQuery({
